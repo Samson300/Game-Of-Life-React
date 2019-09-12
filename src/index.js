755759029dde15d030aba955137584c09a4dcb69
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-11 20:12:37 -0400

    adds Grid class and displays Generations as 0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Grid extends React.Component {
+    render() {
+        return(
+            <div>
+                Grid
+            </div>
+        );
+    }
+}
+
 class Main extends React.Component {
     constructor() {
         super();
@@ -14,7 +24,8 @@ class Main extends React.Component {
         return (
             <div>
                 <h1>The Game Of Life</h1>
-                
+                <Grid />
+                    <h2>Generations: {this.state.generation}</h2>
             </div>
         );
     }
